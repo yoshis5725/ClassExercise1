14e6b102c4eb5f544b4d3acfcebf250cff399e66
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-10 14:44:12 -0600

    completed exercise1

---
+++ ClassExercise/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ClassExercise
+{
+	public class Car
+	{
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
+
---
+++ ClassExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car()
+            {
+                Make = "BMW",
+                Model = "528i",
+                Year = 2012
+            };
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+        }
+    }
+}
+
